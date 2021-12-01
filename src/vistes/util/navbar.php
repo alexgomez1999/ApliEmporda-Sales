b578d52019ee6522c044daac7ef2de30d55c5001
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-12-01 11:50:52 +0100

    canvis

--- src/vistes/util/navbar.php
+++ src/vistes/util/navbar.php
@@ -1,12 +1,11 @@
 <nav>
     <ul>
-        <li><i class="fa fa-home"></i> Inici</li>
-        <li><i class="fa fa-calendar"></i> Calendari</li>
+        <li><i class="fa fa-home"></i> ApliSales</li>
+        <li>Calendari</li>
         <li><a href="#"><i class="fas fa-tachometer-alt"></i></a></li>
         <li><a href="#"><i class="fas fa-file-alt"></i></a></li>
         <li><a href="#"><i class="fas fa-person-booth"></i></a></li>
         <li><a href="#"><i class="fas fa-calendar"></i></a></li>
-        <li><p><?= $_SESSION['login']['Usuari'] ?></p></li>
+        <li><?= $_SESSION['login']['Usuari'] ?></li>
     </ul>
 </nav>
-<input type="text" id="marginHidden">
