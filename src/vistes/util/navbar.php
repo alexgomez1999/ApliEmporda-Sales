4765ea0f977b53efc05b49a0c1321a28ecc50070
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-12-02 10:18:15 +0100

    canvis en el menu

--- src/vistes/util/navbar.php
+++ src/vistes/util/navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg">
+<nav id="menu" class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><i class="fa fa-home"></i> ApliSales</a>
+    <a class="navbar-brand" href="index.php"><i class="fa fa-home"></i> ApliSales</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -16,10 +16,18 @@
             <a href="#" class="nav-link"><i class="fa fa-person-booth"></i></a>
         </li>
         <li class="nav-item">
-            <a href="#" class="nav-link"><i class="fa fa-calendar"></i></a>
+            <a href="index.php?r=calendari" class="nav-link"><i class="fa fa-calendar"></i></a>
         </li>
-        <li class="nav-item">
-            <a href="index.php?r=logout" class="nav-link"><?= $_SESSION['login']['Usuari'] ?></a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="navbarScrollingDropdown" href="#">
+            <i class="fa fa-user-circle"></i> <?= $_SESSION['login']['Usuari'] ?>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li><a class="dropdown-item" href="index.php?r=elMeuPerfil"><i class="fa fa-id-badge"></i> El Meu Perfil</a></li>
+              <li><a class="dropdown-item" href="index.php?r=lesMevesSales"><i class="fa fa-person-booth"></i> Les Meves Sales</a></li>
+              <li><hr class="dropdown-separacio"></li>
+              <li><a class="dropdown-item" href="index.php?r=logout"><i class="fa fa-sign-out-alt"></i> Tanca la Sessió</a></li>
+          </ul>
         </li>
       </ul>
     </div>
