617bbc700be889517ef98590be4e16a41aefbd8d
Àlex Gómez <alexalbons@hotmail.com>
2021-12-03 13:45:00 +0100

    New structure started

--- src/vistes/index.php
+++ src/vistes/index.php
@@ -9,37 +9,26 @@
     <?php if($_SESSION['login']['Rol'] != 'Administrador') { ?>
       <!-- VISTA USUARI NORMAL -->
       <div class="index-user-container">
-      <table class="table crud-table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Sala</th>
-      <th scope="col">Centre</th>
-      <th scope="col">Ubicació</th>
-      <th scope="col">Data</th>
-      <th scope="col">Hora entrada</th>
-      <th scope="col">Hora sortida</th>
-    </tr>
-  </thead>
-  <tbody>
-    <!-- FILES DINÀMIQUES CONFORME SALES TINGUI RESERVADES L'USUARI -->
-    <?php foreach ($sala as $row) { ?>
-    <tr>
-      <th scope="row"><?= $row['Codi'] ?></th>
-      <td><?= $row['Nom'] ?></td>
-      <td><?= $row['Centre'] ?></td>
-      <td><?= $row['Ubicacio'] ?></td>
-      <td><?= $row['Data'] ?></td>
-      <td><?= $row['HoraInici'] ?></td>
-      <td><?= $row['HoraFi'] ?></td>
-      <td><a href="index.php?r=deleteReservation&id=<?= $row['Id'] ?>">Eliminar</a></td>
-    </tr>
-    <?php } ?>
-  </tbody>
-</table>
-<a href="index.php?r=newReservation" class="reservarBtn" style="text-decoration:none"><i class="fas fa-plus"></i></a>
-      </div>
-
+        <?php foreach ($sala as $row) { ?>
+        <div class="index-user-container2">
+          <img src="https://www.desarrollolibre.net/public/broken-img.png" alt="" class="imatge-sala">
+          <div class="column-container">
+            <b><?= $row['Nom'] ?></b>
+            <p><?= $row['Ubicacio'] ?>, <?= $row['Centre'] ?></p>
+          </div>
+          <div class="column-container">
+            <b>Data</b>
+            <p><?= $row['Data'] ?></p>
+          </div>
+          <div class="column-container">
+            <b>Hora</b>
+            <p>De:  <?= $row['HoraInici'] ?> A: <?= $row['HoraFi'] ?></p>
+          </div>
+          <a href="index.php?r=deleteReservation&id=<?= $row['Id'] ?>">Eliminar</a>
+          <a href="index.php?r=newReservation" class="reservarBtn" style="text-decoration:none"><i class="fas fa-plus"></i></a>
+        </div>
+        <?php } ?>
+        </div>
       <?php } else { ?>
         
       <!-- VISTA ADMINISTRADOR -->
