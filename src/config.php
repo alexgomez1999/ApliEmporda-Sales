d1958dce481c76ddba28559c54bafb96764c46af
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-01-17 12:21:02 +0100

    millores en el fitxer index.php

--- src/config.php
+++ src/config.php
@@ -25,10 +25,27 @@ $config["db"]["pass"] = '@dminProgramacio%2022';
 $config["db"]["dbname"] = 'applisales';
 $config["db"]["host"] = '185.66.41.58';
 
+/* inclou els models de l'aplicació */
 require_once "../src/emeset/peticio.php";
 require_once "../src/emeset/resposta.php";
 require_once "../src/emeset/contenidor.php";
-
 require_once "../src/models/connexio.php";
 require_once "../src/models/usuarisPDO.php";
 require_once "../src/models/salesPDO.php";
+
+/* inclou els controladors de l'aplicació */
+include "../src/controladors/index.php";
+include "../src/controladors/login.php";
+include "../src/controladors/dologin.php";
+include "../src/controladors/logout.php";
+include "../src/controladors/userProfile.php";
+include "../src/controladors/calendari.php";
+include "../src/controladors/newReservation.php";
+include "../src/controladors/eliminarReserva.php";
+include "../src/controladors/ajaxServer.php";
+include "../src/controladors/llistaReserves.php";
+include "../src/controladors/salesDisponibles.php";
+
+/* inclou els middleware de l'aplicació */
+include "../src/middleware/middleAdmin.php";
+include "../src/middleware/middleLogin.php";
