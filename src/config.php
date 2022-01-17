ae06cd52cb0794efc929eb562623ee02a436f3dc
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-01-17 12:11:15 +0100

    canvis menors

--- src/config.php
+++ src/config.php
@@ -19,16 +19,16 @@
 $config = array();
 
 /* configuració de connexió a la base dades */
-// $config["db"] = array();
-// $config["db"]["user"] = 'programacio2';
-// $config["db"]["pass"] = '@dminProgramacio%2022';
-// $config["db"]["dbname"] = 'applisales';
-// $config["db"]["host"] = '185.66.41.58';
+$config["db"] = array();
+$config["db"]["user"] = 'programacio2';
+$config["db"]["pass"] = '@dminProgramacio%2022';
+$config["db"]["dbname"] = 'applisales';
+$config["db"]["host"] = '185.66.41.58';
 
-// require_once "../src/emeset/peticio.php";
-// require_once "../src/emeset/resposta.php";
-// require_once "../src/emeset/contenidor.php";
+require_once "../src/emeset/peticio.php";
+require_once "../src/emeset/resposta.php";
+require_once "../src/emeset/contenidor.php";
 
-// require_once "../src/models/connexio.php";
-// require_once "../src/models/usuarisPDO.php";
-// require_once "../src/models/salesPDO.php";
+require_once "../src/models/connexio.php";
+require_once "../src/models/usuarisPDO.php";
+require_once "../src/models/salesPDO.php";
