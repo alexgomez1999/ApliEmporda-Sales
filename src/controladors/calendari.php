d5ec5117bfc5294f8645c7e84483d3c32fc510a8
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-01-17 10:13:36 +0100

    canvis menors

--- src/controladors/calendari.php
+++ src/controladors/calendari.php
@@ -2,10 +2,6 @@
 
 function ctrlCalendari($peticio, $resposta, $contenidor)
 {
-    $usuarisPDO = $contenidor->usuaris();
-
-    $usuarisPDO->delete();
-
     $resposta->SetTemplate("calendari.php");
     return $resposta;
 }
