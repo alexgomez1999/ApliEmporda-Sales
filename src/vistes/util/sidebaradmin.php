5dbd610bee752a6c0fca19e6f8a209fc88908982
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-01-21 12:54:29 +0100

    canvis menors

--- src/vistes/util/sidebaradmin.php
+++ src/vistes/util/sidebaradmin.php
@@ -1,57 +1,57 @@
-<div id="sidebaradmin" class="col-md-2 d-flex flex-column flex-shrink-0 p-3">
+<div id="sidebaradmin" class="d-flex flex-column flex-shrink-0 p-3">
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
         <?php if ($_REQUEST["info"] === "usuaris") { ?>
         <a href="index.php?info=usuaris" class="nav-link text-white itemsidebaractive">
-          Usuaris
+          <i class="fas fa-users"></i>
         </a>
         <?php } else { ?>
         <a href="index.php?info=usuaris" class="nav-link text-white">
-          Usuaris
+          <i class="fas fa-users"></i>
         </a>
         <?php } ?>
       </li>
       <li>
       <?php if ($_REQUEST["info"] === "sales") { ?>
         <a href="index.php?info=sales" class="nav-link text-white itemsidebaractive">
-          Sales
+          <i class="fas fa-person-booth"></i>
         </a>
       <?php } else { ?>
         <a href="index.php?info=sales" class="nav-link text-white">
-          Sales
+          <i class="fas fa-person-booth"></i>
         </a>
       <?php } ?>
       </li>
       <li>
       <?php if ($_REQUEST["info"] === "recursos") { ?>
         <a href="index.php?info=recursos" class="nav-link text-white itemsidebaractive">
-          Recursos
+          <i class="fas fa-clipboard-list"></i>
         </a>
       <?php } else { ?>
         <a href="index.php?info=recursos" class="nav-link text-white">
-          Recursos
+          <i class="fas fa-clipboard-list"></i>
         </a>
       <?php } ?>
       </li>
       <li>
       <?php if ($_REQUEST["info"] === "reserves") { ?>
         <a href="index.php?info=reserves" class="nav-link text-white itemsidebaractive">
-          Reserves
+          <i class="fas fa-concierge-bell"></i>
         </a>
       <?php } else { ?>
         <a href="index.php?info=reserves" class="nav-link text-white">
-          Reserves
+          <i class="fas fa-concierge-bell"></i>
         </a>
       <?php } ?>
       </li>
       <li>
       <?php if ($_REQUEST["info"] === "incidencies") { ?>
         <a href="index.php?info=incidencies" class="nav-link text-white itemsidebaractive">
-          Incidències
+          <i class="fas fa-file-alt"></i>
         </a>
       <?php } else { ?>
         <a href="index.php?info=incidencies" class="nav-link text-white">
-          Incidències
+          <i class="fas fa-file-alt"></i>
         </a>
       <?php } ?>
       </li>
